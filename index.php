dc9be6687972ebc98f36176c4397e0f161b794f6
Muhamad Zahrawani Syaban <if23.muhamadsyaban@mhs.ubpkarawang.ac.id>
2024-10-20 16:03:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+</head>
+    <h1>Login</h1>
+        <form action="./backend/login.php" method="POST">
+        <input type="email" name="email" id="email" placeholder="email"/>
+        <input type="password" name="password" id="password" placeholder="password"/>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
